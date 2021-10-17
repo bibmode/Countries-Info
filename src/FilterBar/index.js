7dd2efc9029167ae82b2b90a7bb4d9f20725749d
Genevieve <vievenavales29@gmail.com>
2021-10-17 21:31:25 +0800

    search input functionality

--- src/FilterBar/index.js
+++ src/FilterBar/index.js
@@ -1,12 +1,19 @@
 import { Container, SearchBar } from "./FilterBar.styles";
 import SearchIcon from "@mui/icons-material/Search";
+import { useState } from "react";
 
-const FilterBar = () => {
+const FilterBar = ({ searchCountry }) => {
+  // const [value, setValue] = useState("");
+
   return (
     <Container>
       <SearchBar>
         <SearchIcon className="searchIcon" />
-        <input type="text" placeholder="Search for a country.." />
+        <input
+          onKeyDown={(e) => searchCountry(e)}
+          type="text"
+          placeholder="Search for a country.."
+        />
       </SearchBar>
     </Container>
   );
--- src/HomeContainer/index.js
+++ src/HomeContainer/index.js
@@ -7,6 +7,7 @@ import { Container, Grid, Wrapper } from "./HomeContainer.styles";
 const HomeContainer = () => {
   const [countries, setCountries] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [search, setSearch] = useState(null);
 
   useEffect(() => {
     fetch("https://restcountries.com/v3.1/all")
@@ -18,10 +19,32 @@ const HomeContainer = () => {
       });
   }, []);
 
+  useEffect(() => {
+    fetch(
+      `${
+        search
+          ? `https://restcountries.com/v3.1/name/${search}`
+          : "https://restcountries.com/v3.1/all"
+      }`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        setLoading(true);
+        setCountries(data);
+        setLoading(false);
+      });
+  }, [search]);
+
+  const searchCountry = (e) => {
+    if (e.keyCode === 13) {
+      setSearch(e.target.value);
+    }
+  };
+
   return (
     <Container>
       <Wrapper>
-        <FilterBar />
+        <FilterBar searchCountry={searchCountry} />
         <Grid>
           {!loading ? (
             countries.map((country) => (
